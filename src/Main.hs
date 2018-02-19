30145f0dfc5dffb5e0653a1ab76ceb8944b79cf7
SirFurness <eric.1334@icloud.com>
2018-02-18 17:34:18 -0800

    Multiple Problems

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 import Control.Monad
 import Control.Monad.State
+import Data.Random
+import Data.Random.Extras
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core hiding (get)
 import System.Random
@@ -30,20 +32,33 @@ nextBtn = do
   return [output, view]
 
 getNext :: UI String
-getNext = sumOfGeoSeries
+getNext = pick [sumOfGeoSeries, sumOfInfiniteGeoSeries]
+
+pick :: [UI String] -> UI String
+pick list = do
+  gen <- liftIO newStdGen
+  let [I n] = genRandomNums [((0, length list - 1), I)] gen
+  list !! n
 
 sumOfGeoSeries :: UI String
 sumOfGeoSeries = do
   gen <- liftIO newStdGen
-  let ((n, a, r), _) = runState genThreeRandom gen
+  --let (n, a, r) = evalState genThreeRandom gen
+  let [I n, I a, D r] = genRandomNums [((1, 10), I), ((0, 25), I), ((1, 500), (\x -> D $ fromIntegral x/100))] gen
   return ("Find the sum of the first " ++ show n ++ " numbers in the geometric series if a1 = " ++ show a ++ " and r = " ++ show r)
 
-genThreeRandom :: State StdGen (Int, Int, Float)
-genThreeRandom = do
-  n <- genRandom (1, 10)
-  a <- genRandom (0, 25)
-  r <- genRandom (1, 500)
-  return (n, a, fromInteger r/100)
+sumOfInfiniteGeoSeries :: UI String
+sumOfInfiniteGeoSeries = do
+  gen <- liftIO newStdGen
+  let [I a, D r] = genRandomNums [((1, 25), I), ((-9, 9), (\x -> D $ fromIntegral x/10))] gen
+  return ("Find the sum of the infinite geometric series if a1 = " ++ show a ++ " and r = " ++ show r)
+
+data Number = I Int | D Double deriving Show
+
+genRandomNums :: [((Int, Int), (Int -> Number))] -> StdGen -> [Number]
+genRandomNums list = evalState (mapM (\(range, f) -> do
+                                     n <- genRandom range
+                                     return $ f n) list)
 
 genRandom :: (Random a) => (a, a) -> State StdGen a
 genRandom range = do
@@ -51,4 +66,4 @@ genRandom range = do
   let (a, g) = randomR range gen
   put g
   return a
- 
+  
